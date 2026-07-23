93da454a04f7ffee90d4c46fb27042011425ecac
Light Play <lightplay456@gmail.com>
2026-07-23 23:54:25 +0300

    fix: исправлены проблемы связанные с запуском контейнера

--- services/notifier/config/runtime.exs
+++ services/notifier/config/runtime.exs
@@ -1,13 +1,21 @@
 import Config
 
-Dotenvy.source!(".env")
+config :swoosh, :api_client, false
+
+smtp_relay = System.get_env("SMTP_RELAY") || raise "SMTP_RELAY not set"
+smtp_username = System.get_env("SMTP_USERNAME") || raise "SMTP_USERNAME not set"
+smtp_password = System.get_env("SMTP_PASSWORD") || raise "SMTP_PASSWORD not set"
+smtp_from = System.get_env("SMTP_FROM_EMAIL") || raise "SMTP_FROM_EMAIL not set"
+kafka_brokers = System.get_env("KAFKA_BROKERS") || raise "KAFKA_BROKERS not set"
+kafka_topic = System.get_env("KAFKA_TOPIC") || raise "KAFKA_TOPIC not set"
+kafka_group = System.get_env("KAFKA_GROUP_ID") || raise "KAFKA_GROUP_ID not set"
 
 config :notifier, Notifier.Mailer,
   adapter: Swoosh.Adapters.SMTP,
-  relay: Dotenvy.env!("SMTP_RELAY"),
-  port: 465,
-  username: Dotenvy.env!("SMTP_USERNAME"),
-  password: Dotenvy.env!("SMTP_PASSWORD"),
+  relay: smtp_relay,
+  port: String.to_integer(System.get_env("SMTP_PORT") || "465"),
+  username: smtp_username,
+  password: smtp_password,
   ssl: true,
   tls: :never,
   auth: :always,
@@ -24,9 +32,9 @@ config :notifier, Notifier.Mailer,
     server_name_indication: ~c"smtp.yandex.ru"
   ]
 
-config :notifier, :from_email, Dotenvy.env!("SMTP_FROM_EMAIL")
+config :notifier, :from_email, smtp_from
 
 config :notifier, Notifier.KafkaConsumer,
-  brokers: Dotenvy.env!("KAFKA_BROKERS"),
-  topic: Dotenvy.env!("KAFKA_TOPIC"),
-  group_id: Dotenvy.env!("KAFKA_GROUP_ID")
+  brokers: kafka_brokers,
+  topic: kafka_topic,
+  group_id: kafka_group
